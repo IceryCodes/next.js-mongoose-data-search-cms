de08097be05c255b5f31624eca5659b7abfddb4f
Icery <Icery@Icery.tw>
2025-01-21 13:10:21 +0800

    feat(update-ui-and-search-logic): hide register page.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,21 +1,22 @@
 import { ReactElement } from 'react';
 
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 
-import RegisterContent from '@/app/register/components/RegisterContent';
-import { getPageUrlByType, PageType } from '@/domains/interfaces';
-import { metadataInfo } from '@/domains/metadatas';
+// import RegisterContent from '@/app/register/components/RegisterContent';
+// import { getPageUrlByType, PageType } from '@/domains/interfaces';
+// import { metadataInfo } from '@/domains/metadatas';
 
-export const generateMetadata = async (): Promise<Metadata> => {
-  return metadataInfo({
-    pageName: PageType.REGISTER,
-    currentPath: `${process.env.NEXT_PUBLIC_BASE_URL}${getPageUrlByType(PageType.REGISTER)}`,
-  });
-};
+// export const generateMetadata = async (): Promise<Metadata> => {
+//   return metadataInfo({
+//     pageName: PageType.REGISTER,
+//     currentPath: `${process.env.NEXT_PUBLIC_BASE_URL}${getPageUrlByType(PageType.REGISTER)}`,
+//   });
+// };
 
 const Page = (): ReactElement => (
   <div className={`mt-52 md:mt-0 md:h-content flex items-center`}>
-    <RegisterContent />
+    <label className="w-full text-center">暫不開放註冊</label>
+    {/* <RegisterContent /> */}
   </div>
 );
 export default Page;
